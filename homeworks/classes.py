c616af6b58665a4942c70aa3e1cec0fe84c86614
novikevichpavel <novikevichp@gmail.com>
2026-03-16 22:10:29 +0300

    Update

--- homeworks/classes.py
+++ homeworks/classes.py
@@ -6,13 +6,21 @@ class Image:
 
     def resize_image(self, new_resolution):
         self.resolution = new_resolution
+        return self.resolution
 
+    @staticmethod
+    def create_message(tesx_one, text_two):
+        return f"{tesx_one} {text_two}"
+    
     def __str__(self):
-        return f"The image {self.title}.{self.extension} is {self.resolution}"
+        return f"Image {self.title}.{self.extension} has resolution: {self.resolution}"
+        
 
     
 my_image = Image("1920x1024", "My_cat", "png")
-my_image.__str__()
 
-my_image.resize_image("1024x680")
-print(my_image)
+m_1 = my_image.create_message("Hello", "World")
+print(m_1)
+
+print(my_image.resize_image("1024x680"))
+print(my_image.__str__())
