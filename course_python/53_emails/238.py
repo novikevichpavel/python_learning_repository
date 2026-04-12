70c8c26fd816a96c90e0f20dcf04ec9a37cd14c4
novikevichpavel <novikevichp@gmail.com>
2026-04-12 22:41:45 +0300

    Add new files

--- course_python/53_emails/238.py
+++ course_python/53_emails/238.py
@@ -1,46 +1,55 @@
 from email.message import EmailMessage
 import smtplib
+from string import Template
+from pathlib import Path
 
-# my_email = EmailMessage()
+my_email = EmailMessage()
 
-# my_email["from"] = "Pavel"
-# my_email["to"] = "novikevichp@gmail.com"
-# my_email["subject"] = "Hello from Python"
-# my_email.set_content("Hey. How are you doing?")
+html_template = Template(Path("/Users/pavel/personal/py_learn/course_python/53_emails/templates/index.html").read_text())
+html = html_template.substitute({"name": "Pavel", "date": "tommorow"})
 
-# with smtplib.SMTP(host="localhost", port=2525) as message:
-#     message.ehlo()
-#     message.send_message(my_email)
-#     print("Email was sent")
+my_email["from"] = "Pavel"
+my_email["to"] = "novikevichp@gmail.com"
+my_email["subject"] = "Let's go out"
+my_email.set_content(html, "html")
+
+with open("/Users/pavel/personal/py_learn/course_python/53_emails/images/aaa.jpg", "rb") as my_img:
+    image_data = my_img.read()
+    my_email.add_attachment(image_data, maintype="image", subtype="jpg", filename="aaa.jpg")
+
+with smtplib.SMTP(host="localhost", port=2525) as message:
+    message.ehlo()
+    message.send_message(my_email)
+    print("Email was sent")
 
 
-class EmailSender:
-    def __init__(self, host: str, port: int):
-        self.localhost = host
-        self.port = port
+# class EmailSender:
+#     def __init__(self, host: str, port: int):
+#         self.localhost = host
+#         self.port = port
 
-    def create_message(self, sender: str, recipient: str, content: str, body: str):
-        message = EmailMessage()
-        message["from"] = sender
-        message["to"] = recipient
-        message["subject"] = content
-        message.set_content(body)
-        return message
+#     def create_message(self, sender: str, recipient: str, content: str, body: str):
+#         message = EmailMessage()
+#         message["from"] = sender
+#         message["to"] = recipient
+#         message["subject"] = content
+#         message.set_content(body)
+#         return message
 
-    def send_message(self, message: EmailMessage):
-        with smtplib.SMTP(host=self.localhost, port=self.port) as smtp_server:
-            smtp_server.ehlo()
-            smtp_server.send_message(message)
-            print("Email was sent")
+#     def send_message(self, message: EmailMessage):
+#         with smtplib.SMTP(host=self.localhost, port=self.port) as smtp_server:
+#             smtp_server.ehlo()
+#             smtp_server.send_message(message)
+#             print("Email was sent")
 
-sender = EmailSender(host="localhost", port=2525)
+# sender = EmailSender(host="localhost", port=2525)
 
-email = sender.create_message(
-    sender="Pavel",
-    recipient="Polina",
-    content="Hello message",
-    body="Hello^ my dear. How are you?"
-)
+# email = sender.create_message(
+#     sender="Pavel",
+#     recipient="Polina",
+#     content="Hello message",
+#     body="Hello^ my dear. How are you?"
+# )
 
-sender.send_message(email)
+# sender.send_message(email)
 
---
+++ course_python/54_data_base/242.py
@@ -0,0 +1,84 @@
+DB_NAME = "sql_db.db"
+
+# with sqlite3.connect(DB_NAME) as sql:
+#     print(sql)
+#     print(sqlite3.version)
+
+# with sqlite3.connect(DB_NAME) as sql:
+#     sql_req = """CREATE TABLE IF NOT EXISTS courses (
+#     id integer PRIMARY KEY,
+#     title text NOT NULL,
+#     students_qty integer,
+#     reviews_qty integer
+#     );"""
+#     sql.execute(sql_req)
+
+# courses = [
+#     (2, "JavaScript course", 0, 0),
+#     (3, "Java", 1, 0),
+#     (4, "C# course", 0, 0)
+# ]
+
+# with sqlite3.connect(DB_NAME) as sql:
+#     sql_req = "INSERT INTO courses VALUES (?, ?, ?, ?)"
+#     # sql.execute(sql_req, (1, "Python Course", 1, 1))
+#     for course in courses:
+#         sql.execute(sql_req, course)
+#     sql.commit()
+
+# with sqlite3.connect(DB_NAME) as sql:
+#     sql_req = "SELECT * FROM courses WHERE students_qty > 0"
+#     sql_data = sql.execute(sql_req)
+#     for i in sql_data:
+#         print(i[1])
+#     record = sql_data.fetchall()
+#     print(record)
+
+
+import sqlite3
+
+class Course:
+    DB_NAME = "new_data_base.db"
+    saved_data = []
+
+    def __init__(self):
+        self.data_base = self.DB_NAME
+
+    def create_data_base(self):
+        with sqlite3.connect(self.data_base) as sql_db:
+            sql_req = """CREATE TABLE IF NOT EXISTS my_courses (
+            id integer PRIMARY KEY,
+            course_name text NOT NULL,
+            progress integer
+            );"""
+            sql_db.execute(sql_req)
+
+    def insert_values(self):
+        with sqlite3.connect(self.data_base) as sql_db:
+            sql_req = "INSERT INTO my_courses VALUES(?, ?, ?)"
+            for i in self.saved_data:
+                sql_db.execute(sql_req, i)
+            sql_db.commit()
+
+    def get_course_data(self):
+        for i in range(3):
+            course_id = int(input("Enter course id: "))
+            course_title = input("Enter course name: ")
+            course_progress = int(input("Enter your porgress: "))
+            saved_tuple = (course_id, course_title, course_progress)
+            self.saved_data.append(saved_tuple)
+        print(self.saved_data)
+
+    def get_db_writes(self):
+        with sqlite3.connect(self.data_base) as sql_db:
+            sql_req = "SELECT * FROM my_courses"
+            sql_data = sql_db.execute(sql_req)
+            for i in sql_data:
+                print(i)
+
+sql = Course()
+sql.create_data_base()
+sql.get_course_data()
+sql.insert_values()
+sql.get_db_writes()
+        
